2707bdf83126ed7380d5c773d98877489f72d892
Ethan Knox <ethan.knox@health-union.com>
2019-12-30 12:22:13 -0500

    base conn string builder

--- snowshu/adapters/base_sql_adapter.py
+++ snowshu/adapters/base_sql_adapter.py
@@ -10,7 +10,7 @@ class BaseSQLAdapter:
     ALLOWED_CREDENTIALS:iter
 
     def __init__(self):
-        self.class_name=self.__class__.__name__
+        self.CLASSNAME=self.__class__.__name__
 
     @property
     def credentials(self)->dict:
@@ -20,10 +20,10 @@ class BaseSQLAdapter:
     def credentials(self,value:Credentials)->None:
         for cred in self.REQUIRED_CREDENTIALS:
             if value.__dict__[cred] == None:
-                raise KeyError(f"{self.__class__.__name__} requires missing credential {cred}.")
+                raise KeyError(f"{self.CLASSNAME} requires missing credential {cred}.")
         ALL_CREDENTIALS = self.REQUIRED_CREDENTIALS+self.ALLOWED_CREDENTIALS
         for val in [val for val in value.__dict__.keys() if (val not in ALL_CREDENTIALS and value.__dict__[val] is not None)]:
-            raise KeyError(f"{self.__class__.__name__} received extra argument {val} this is not allowed")
+            raise KeyError(f"{self.CLASSNAME} received extra argument {val} this is not allowed")
 
         self._credentials=value
 
@@ -31,8 +31,7 @@ class BaseSQLAdapter:
         if not self._credentials:
             raise KeyError('Adapter.get_connection called before setting Adapter.credentials')
 
-        logger.debug(f'Aquiring {self.__class__.__name__} connection...')
-        super().get_connection()
+        logger.debug(f'Aquiring {self.CLASSNAME} connection...')
         conn_parts=[f"snowflake://{self.credentials.user}:{self.credentials.password}@{self.credentials.account}/{self.credentials.database}/"]
         conn_parts.append(self.credentials.schema if self.credentials.schema is not None else '')
         get_args=list()
@@ -47,3 +46,18 @@ class BaseSQLAdapter:
         logger.debug('Done. New snowflake connection aquired.')
         logger.debug(f'conn string: {repr(engine.url)}')
         return engine
+
+
+    def _build_conn_string(self)->str:
+        """ This is the most basic implementation of a connection string possible and is intended to be extended.
+            generates a database url per https://docs.sqlalchemy.org/en/13/core/engines.html#database-urls
+        """
+        if not hasattr(self,'dialect'):
+            # attempt to infer the dialect
+            raise KeyError('base_sql_adapter unable to build connection string; required param `dialect` to infer.')
+
+        self._credentials.urlencode()
+        conn_string=f"{self.dialect}://{self._credentials.user}:{self._credentials.password}@{self._credentials.host}/{self._credentials.database}?"
+        get_args='&'.join([f"{arg}={self._credentials.__dict__[arg]}" for arg in self.ALLOWED_CREDENTIALS if arg in vars(self._credentials) and self._credentials.__dict__[arg] is not None])
+        return conn_string+get_args
+                                
--- tests/unit/test_base_adapter.py
+++ tests/unit/test_base_adapter.py
@@ -1,8 +1,13 @@
 import pytest
 from snowshu.adapters import BaseSQLAdapter
 from tests.common import rand_string
-from snowshu.core.models.credentials import Credentials, USER,PASSWORD,HOST,ACCOUNT,SCHEMA,DATABASE
+from snowshu.core.models.credentials import Credentials, USER,PASSWORD,HOST,ACCOUNT,SCHEMA,DATABASE,ROLE
 
+
+
+def rand_creds(args)->Credentials:
+    kwargs=dict(zip(args,[rand_string(10) for _ in range(len(args))]))
+    return Credentials(**kwargs)
 
 def test_sets_credentials():
 
@@ -10,10 +15,6 @@ def test_sets_credentials():
 
     base.REQUIRED_CREDENTIALS=(USER,PASSWORD,HOST)
     base.ALLOWED_CREDENTIALS=(ACCOUNT,SCHEMA)
-    
-    def rand_creds(args)->Credentials:
-        kwargs=dict(zip(args,[rand_string(10) for _ in range(len(args))]))
-        return Credentials(**kwargs)
 
     with pytest.raises(KeyError):
         base.credentials=rand_creds((HOST,))
@@ -24,3 +25,16 @@ def test_sets_credentials():
     base.credentials=rand_creds((USER,PASSWORD,HOST,))
     
     base.credentials=rand_creds((USER,PASSWORD,HOST,ACCOUNT,))
+
+
+def test_default_conn_string():
+    base=BaseSQLAdapter()
+    base.dialect='postgres'
+    
+    base.REQUIRED_CREDENTIALS=(USER,PASSWORD,DATABASE,HOST)
+    base.ALLOWED_CREDENTIALS=(ROLE,SCHEMA,ACCOUNT)
+  
+    creds=rand_creds((USER,PASSWORD,HOST,DATABASE,ACCOUNT,))
+    base.credentials=creds
+    
+    assert base._build_conn_string() == f'postgres://{creds.user}:{creds.password}@{creds.host}/{creds.database}?account={creds.account}'
