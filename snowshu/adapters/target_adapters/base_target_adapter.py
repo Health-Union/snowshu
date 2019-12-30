3e7826c6253cefb06252a32669c7655cc74604f8
Ethan Knox <ethan.knox@health-union.com>
2019-12-30 14:43:21 -0500

    base adapter builds sql stacked

--- snowshu/adapters/target_adapters/base_target_adapter.py
+++ snowshu/adapters/target_adapters/base_target_adapter.py
@@ -1,14 +1,35 @@
+from snowshu.adapters import BaseSQLAdapter
 from snowshu.core.models import Relation
 
 
-class BaseTargetAdapter:
+class BaseTargetAdapter(BaseSQLAdapter):
     """All target adapters inherit from this one."""
+
+    DATA_TYPE_MAPPINGS=dict()
     
     def create_relation(self,relation:Relation)->bool:
         """creates the relation in the target, returns success"""
-        raise NotImplementedError()
-
+        ddl_statement=f"CREATE {relation.materialization} {relation.quoted_dot_notation} ("
+        for attr in relation.attributes:
+            dtype=list(self.DATA_TYPE_MAPPINGS.keys())[list(self.DATA_TYPE_MAPPINGS.values()).index(attr.data_type)]
+            ddl_statement+=f"\n {attr.name} {dtype},"
+        ddl_statement=ddl_statement[:-1]+"\n)"
+        full_statement=';\n'.join((self._create_database_if_not_exists(),
+                        self._create_schema_if_not_exists(),
+                        ddl_statement,))
+        self._safe_execute(full_statement)
 
     def insert_into_relation(self,relation:Relation)->bool:
         """inserts the data from a relation object into the matching target relation, returns success"""
         raise NotImplementedError()
+
+    def _create_database_if_not_exists(self, database:str)->str:
+        return f"CREATE SCHEMA IF NOT EXISTS '{database}'"
+
+    def _create_schema_if_not_exists(self, schema:str)->str:
+        return f"CREATE SCHEMA IF NOT EXISTS '{schema}'"
+
+    def _safe_execute(self,query:str)->None:
+        pass
+
+
---
+++ tests/unit/test_base_target_adapter.py
@@ -0,0 +1,35 @@
+import pytest
+import mock
+from snowshu.core.models import Relation
+from snowshu.core.models import data_types as dtypes
+from snowshu.core.models import materializations as mz
+from snowshu.adapters.target_adapters import BaseTargetAdapter
+
+@mock.patch('snowshu.adapters.target_adapters.base_target_adapter.BaseTargetAdapter._safe_execute')
+def test_creates_relation_sets_up(_safe_execute,stub_relation):
+    base=BaseTargetAdapter()
+    db='create database if not exists'
+    schema='create schema if not exists'
+    base.DATA_TYPE_MAPPINGS=dict(rocks=dtypes.INTEGER,
+                            banana=dtypes.DOUBLE,
+                            soup=dtypes.VARCHAR)
+    base.MATERIALIZATION_MAPPINGS=dict(sock=mz.TABLE)
+    
+    base._create_database_if_not_exists=mock.MagicMock(return_value=db)
+    base._create_schema_if_not_exists=mock.MagicMock(return_value=schema)
+    base.create_relation(stub_relation)
+    ddl="""
+CREATE sock IF NOT EXISTS
+    {stub_relation.quoted_dot_notation}    
+(int_attr rocks,
+double_attr banana,
+string_attr soup
+);
+"""
+
+    EXPECTED_STATEMENT=';\n'.join((db,schema, ddl,))
+    
+    assert _safe_execute.called_with(ddl)
+
+
+    
