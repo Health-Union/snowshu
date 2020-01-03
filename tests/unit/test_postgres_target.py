fcac8337989cc980a4cb752eb0d216e96978ca2d
Ethan Knox <ethan.knox@health-union.com>
2020-01-03 13:31:07 -0500

    untyped works :shaka:

--- tests/unit/test_postgres_target.py
+++ tests/unit/test_postgres_target.py
@@ -8,31 +8,27 @@ from snowshu.adapters.target_adapters import PostgresAdapter
 @mock.patch('snowshu.adapters.base_sql_adapter.sqlalchemy')
 def test_spins_up_container(sqlalchemy,stub_replica_configuration):
     REPLICA_NAME=rand_string(10)
-    base=PostgresAdapter()
-    base.load_config(stub_replica_configuration)
+    pg=PostgresAdapter()
+    pg.load_config(stub_replica_configuration)
     mocked_sqlalchemy=mock.MagicMock()
-    base.get_connection=lambda : mocked_sqlalchemy
+    pg.get_connection=lambda : mocked_sqlalchemy
       
-    base._init_image()
+    pg._init_image()
     for attr in ('user','password','database'):
-        assert base._credentials.__dict__[attr] == 'snowshu'
-    assert base._credentials.host == 'snowshu_target'    
-    assert base._credentials.port == 9999
+        assert pg._credentials.__dict__[attr] == 'snowshu'
+    assert pg._credentials.host == 'snowshu_target'    
+    assert pg._credentials.port == 9999
 
     assert mocked_sqlalchemy.execute.called_with('CREATE DATABASE snowshu; CREATE SCHEMA "snowshu"."snowshu"')
-    ## creates image
-    ## all creds set to "snowshu"
-    ## creates snowshu DB
-    
 
 def test_builds_meta(stub_replica_configuration):
     REPLICA_NAME=rand_string(10)
-    base=PostgresAdapter()
-    base.load_config(stub_replica_configuration)
-    base._init_image()
+    pg=PostgresAdapter()
+    pg.load_config(stub_replica_configuration)
+    pg._init_image()
 
     engine=create_engine("postgres://snowshu:snowshu@snowshu_target:9999/snowshu")
     response=engine.execute('SELECT COUNT(*) FROM "snowshu"."snowshu"."replica_meta"')
-    assert response.fetchall()[0] == 1
+    assert response.fetchall()[0][0] == 1
 
 
