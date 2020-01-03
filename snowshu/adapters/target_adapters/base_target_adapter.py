d8e6efd1fe02fd7a015b6ee4bfff977deb228168
Ethan Knox <ethan.knox@health-union.com>
2020-01-03 13:31:07 -0500

    untyped works :shaka:

--- snowshu/adapters/target_adapters/base_target_adapter.py
+++ snowshu/adapters/target_adapters/base_target_adapter.py
@@ -2,7 +2,7 @@ from time import sleep
 from snowshu.core.utils import key_for_value
 from typing import Optional
 from snowshu.adapters import BaseSQLAdapter
-from snowshu.configs import DOCKER_TARGET_PORT,DOCKER_TARGET_CONTAINER
+from snowshu.configs import DOCKER_TARGET_PORT,DOCKER_TARGET_CONTAINER,DEFAULT_INSERT_CHUNK_SIZE
 from snowshu.core.models.credentials import USER,PASSWORD,HOST,PORT,DATABASE
 from snowshu.core.configuration_parser import ReplicaConfiguration
 from snowshu.core.models import Relation,Credentials,Attribute
@@ -10,6 +10,8 @@ from snowshu.core.models import materializations as mz
 from snowshu.core.models import data_types as dt
 from snowshu.core.docker import SnowShuDocker
 from snowshu.logger import Logger
+from datetime import datetime
+import pandas as pd
 logger=Logger().logger
 
 class BaseTargetAdapter(BaseSQLAdapter):
@@ -32,21 +34,37 @@ class BaseTargetAdapter(BaseSQLAdapter):
     def load_config(self,config:ReplicaConfiguration)->None:
         self.replica_configuration=config    
 
-    def create_relation(self,relation:Relation)->bool:
-        """creates the relation in the target, returns success"""
-        ddl_statement=f"CREATE {relation.materialization} {relation.quoted_dot_notation} ("
-        for attr in relation.attributes:
-            dtype=list(self.DATA_TYPE_MAPPINGS.keys())[list(self.DATA_TYPE_MAPPINGS.values()).index(attr.data_type)]
-            ddl_statement+=f"\n {attr.name} {dtype},"
-        ddl_statement=ddl_statement[:-1]+"\n)"
-        full_statement=';\n'.join((self._create_database_if_not_exists(),
-                        self._create_schema_if_not_exists(),
-                        ddl_statement,))
-        self._safe_execute(full_statement)
-
-    def insert_into_relation(self,relation:Relation)->bool:
-        """inserts the data from a relation object into the matching target relation, returns success"""
+    def create_database_if_not_exists(self, database:str)->str:
         raise NotImplementedError()
+
+    def create_schema_if_not_exists(self, database:str, schema:str)->str:
+        raise NotImplementedError()
+
+    def create_and_load_relation(self,relation)->None:
+        self.create_relation_if_not_exists(relation)
+        self.load_data_into_relation(relation)
+
+    def create_relation_if_not_exists(self,relation:Relation)->None:
+        engine=self.get_connection( database_override=relation.database,
+                                    schema_override=relation.schema)
+
+        materialization=key_for_value(self.MATERIALIZATION_MAPPINGS,relation.materialization)
+        ddl_statement=f"""
+CREATE {materialization} IF NOT EXISTS {relation.quoted_dot_notation} 
+({relation.typed_columns(self.DATA_TYPE_MAPPINGS)})
+"""
+        engine.execute(ddl_statement)
+
+    def load_data_into_relation(self,relation:Relation)->None:
+        engine=self.get_connection(database_override=relation.database,
+                                    schema_override=relation.schema)
+        relation.data.to_sql(   relation.name,
+                                engine,
+                                schema=relation.schema,
+                                if_exists='append',
+                                index=False,
+                                chunksize=DEFAULT_INSERT_CHUNK_SIZE,
+                                method='multi')
 
     def _init_image(self)->None:
         docker=SnowShuDocker()
@@ -89,29 +107,13 @@ class BaseTargetAdapter(BaseSQLAdapter):
                             "replica_meta",                           
                             mz.TABLE,
                             attributes)
+
+        relation.data=pd.DataFrame([dict(created_at=datetime.now(),
+                                         name=self.replica_configuration.name,
+                                         short_description=self.replica_configuration.short_description,
+                                         long_description=self.replica_configuration.long_description)])
+
         self.create_and_load_relation(relation)
 
-    def create_database_if_not_exists(self, database:str)->str:
-        raise NotImplementedError()
 
-    def create_schema_if_not_exists(self, database:str, schema:str)->str:
-        raise NotImplementedError()
-
-    def create_and_load_relation(self,relation)->None:
-        self.create_relation_if_not_exists(relation)
-        self.load_data_into_relation(relation)
-
-    def create_relation_if_not_exists(self,relation:Relation)->None:
-        engine=self.get_connection( database_override=relation.database,
-                                    schema_override=relation.schema)
-
-        materialization=key_for_value(self.MATERIALIZATION_MAPPINGS,relation.materialization)
-        ddl_statement=f"""
-CREATE {materialization} IF NOT EXISTS {relation.quoted_dot_notation} 
-({relation.typed_columns(self.DATA_TYPE_MAPPINGS)})
-"""
-        engine.execute(ddl_statement)
-
-    def load_data_into_relation(self,relation:Relation)->None:
-        pass
-            
+                        
