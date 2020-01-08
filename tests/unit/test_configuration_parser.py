41b2f12c68972a4a809919e67f092d90f16cb137
Ethan Knox <ethan.knox@health-union.com>
2020-01-08 14:44:23 -0500

    beefed up parser testing

--- tests/unit/test_configuration_parser.py
+++ tests/unit/test_configuration_parser.py
@@ -3,39 +3,30 @@ from tests.common import rand_string
 from io import StringIO
 import yaml
 from snowshu.core.configuration_parser import ConfigurationParser
-from snowshu.utils import PACKAGE_ROOT
 import os
 
-def test_fills_in_empty_source_values(stub_configs):
-    stub_configs=stub_configs()
-    mock_config_file=StringIO(yaml.dump(stub_configs))   
-    config_object = ConfigurationParser.from_file_or_path(mock_config_file)
+def test_fills_in_empty_source_values(stub_replica_configuration):
     
-    for rel in config_object.specified_relations:
+    for rel in stub_replica_configuration.specified_relations:
         assert isinstance(rel.unsampled,bool)
-        assert isinstance(rel.relationships,dict)
+        assert isinstance(rel.relationships.bidirectional,list)
 
         for direction in ('bidirectional','directional',):
-            assert direction in rel.relationships.keys()
+            assert getattr(rel.relationships,direction) is not None
 
-@pytest.mark.skip
 def test_fills_empty_top_level_values(stub_configs):
-    parser=ConfigurationParser()
     stub_configs=stub_configs()
     del stub_configs['long_description']
     mock_config_file=StringIO(yaml.dump(stub_configs))   
-    parser.from_file_or_path(mock_config_file)
+    parsed = ConfigurationParser.from_file_or_path(mock_config_file)
 
-    assert parser.long_description == ''
+    assert parsed.long_description == ''
 
-@pytest.mark.skip
 def test_errors_on_missing_section(stub_configs):
-    SOURCE_PROFILE,TARGET_PROFILE,STORAGE_PROFILE=[rand_string(10) for _ in range(3)]
     stub_configs=stub_configs()
     del stub_configs['source']
     with pytest.raises(AttributeError):
-        parser=ConfigurationParser()
         mock_config_file=StringIO(yaml.dump(stub_configs))   
-        parser.from_file_or_path(mock_config_file)
-
+        ConfigurationParser.from_file_or_path(mock_config_file)
+        
 
