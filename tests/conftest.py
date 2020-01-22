d5329f27e90da929adc2e4943868f25c42319f6a
Ethan Knox <ethan.knox@health-union.com>
2020-01-22 11:47:17 -0500

    thinning out conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ import copy
 import json
 import yaml
 from snowshu.core.configuration_parser import ConfigurationParser
+from tests.conftest_modules.test_credentials import CREDENTIALS
+from tests.conftest_modules.test_configuration import CONFIGURATION
 from tests.common import rand_string
 from snowshu.core.models import Relation, Attribute
 import snowshu.core.models.data_types as dt
@@ -12,76 +14,6 @@ import snowshu.core.models.materializations as mz
 import networkx as nx
 import pandas as pd
 from dfmock import DFMock
-
-CONFIGURATION = {
-    "version": "1",
-    "credpath": "tests/assets/integration/credentials.yml",
-    "name": "integration trail path",
-    "short_description": "this is a sample with LIVE CREDS for integration",
-    "long_description": "this is for testing against a live db",
-    "threads": 15,
-    "source": {
-        "profile": "default",
-        "default_sampling": {
-            "databases": [
-                {
-                    "name": "SNOWSHU_DEVELOPMENT",
-                    "schemas": [
-                        {
-                            "name": ".*",
-                            "relations": [
-                                "^(?!.+_VIEW).+$"
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        "include_outliers": True,
-        "sample_method": "bernoulli",
-        "probability": 30,
-        "specified_relations": [
-            {
-                "database": "SNOWSHU_DEVELOPMENT",
-                "schema": "SOURCE_SYSTEM",
-                "relation": "ORDERS",
-                "unsampled": True
-            },
-            {
-                "database": "SNOWSHU_DEVELOPMENT",
-                "schema": "SOURCE_SYSTEM",
-                "relation": "ORDER_ITEMS",
-                "relationships": {
-                    "bidirectional": [
-                        {
-                            "local_attribute": "PRODUCT_ID",
-                            "database": "SNOWSHU_DEVELOPMENT",
-                            "schema": "SOURCE_SYSTEM",
-                            "relation": "PRODUCTS",
-                            "remote_attribute": "ID"
-                        }
-                    ],
-                    "directional": [
-                        {
-                            "local_attribute": "ORDER_ID",
-                            "database": "SNOWSHU_DEVELOPMENT",
-                            "schema": "SOURCE_SYSTEM",
-                            "relation": "ORDERS",
-                            "remote_attribute": "ID"
-                        }
-                    ]
-                }
-            }
-        ]
-    },
-    "target": {
-        "adapter": "postgres"
-    },
-    "storage": {
-        "profile": "default"
-    }
-}
-
 
 @pytest.fixture
 def stub_creds():
