57d69748e9d96b6600dd952eb03a361cba11b1ff
Ethan Knox <ethan.knox@health-union.com>
2020-01-25 19:13:09 -0500

    hacky working specified relations

--- snowshu/core/configuration_parser.py
+++ snowshu/core/configuration_parser.py
@@ -110,8 +110,6 @@ class ConfigurationParser:
                         rel.get('Sampling',None),
                         _build_relationships(rel)) for rel in specified_relations]
 
-
-
         try:
             with open(loadable) as f:
                 logger.debug(f'loading from file {f.name}')
