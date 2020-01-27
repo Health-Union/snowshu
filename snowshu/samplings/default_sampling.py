be7d029ec6beeebf8747b6d06067cb9e2591e3c4
Ethan Knox <ethan.knox@health-union.com>
2020-01-27 11:58:20 -0500

    corrected printable logic

--- snowshu/samplings/default_sampling.py
+++ snowshu/samplings/default_sampling.py
@@ -13,6 +13,9 @@ class DefaultSampling(BaseSampling):
         confidence: The confidence interval to be observed for the sample expressed in a decimal from 0.01 to 0.99 (1% to 99%). Default 0.99 (99%). `http://www.stat.yale.edu/Courses/1997-98/101/confint.htm`
         min_sample_size: The minimum number of records to retrieve from the population. Default 1000.
     """ 
+
+    size:int
+
     def __init__(self,
                  margin_of_error:float=0.02,
                  confidence:float=0.99,
@@ -31,9 +34,10 @@ class DefaultSampling(BaseSampling):
             relation: The :class:`Relation <snowshu.core.models.relation.Relation>` object to prepare.
             source_adapter: The :class:`source adapter <snowshu.adapters.source_adapters.base_source_adapter.BaseSourceAdapter>` instance to use for executing prepare queries. 
         """
-        self.sample_method=BernoulliSampleMethod(
-                                        max(self.sample_size_method.size(
-                                                relation.population_size),
-                                                self.min_sample_size),
-                                        units='rows')
+        self.size=max(self.sample_size_method.size(
+                      relation.population_size),
+                      self.min_sample_size)
+
+        self.sample_method=BernoulliSampleMethod(self.size,
+                                                 units='rows')
 
