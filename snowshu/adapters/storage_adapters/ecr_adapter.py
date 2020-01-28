360a8611a677e60dc111a8c912c60264d2f6f0dd
Ethan Knox <ethan.knox@health-union.com>
2020-01-28 14:37:20 -0500

    refactoring creds to work with storages

--- snowshu/adapters/storage_adapters/ecr_adapter.py
+++ snowshu/adapters/storage_adapters/ecr_adapter.py
@@ -1,6 +1,14 @@
 from snowshu.adapters.storages import BaseStorageAdapter
+import botocredspath as bcp
+import boto3
 
-class ECRAdapter:
+class AWSECRAdapter:
     
     REQUIRED_CREDENTIALS = (REPOSITORY, ACCESS_KEY_ID, SECRET_ACCESS_KEY, REGION)
     ALLOWED_CREDENTIALS = (,)
+
+    def load_config(self, config: Configuration) -> None:
+        self.config = config
+
+    def get_client(self):->boto3.Client:
+        boto3=bcp.update_path(boto3,
