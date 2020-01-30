fe3e40e7bc2e161c380d1cf742ce04a6180dff8a
Ethan Knox <ethan.knox@health-union.com>
2020-01-30 17:21:18 -0500

    list lists

--- snowshu/core/replica/replica_manager.py
+++ snowshu/core/replica/replica_manager.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from dateutil.parser import parse
 from snowshu.logger import Logger
 from snowshu.core.docker import SnowShuDocker
@@ -11,11 +12,17 @@ class ReplicaManager:
     @staticmethod
     def list():
         shdocker=SnowShuDocker()
+        images = shdocker.find_snowshu_images()
+        if len(images) < 1:
+            return "\n\nNo SnowShu replicas found.\n\
+You can create a new replica by running `snowshu create`.\n\n"
+
         collection=[(shdocker.replica_image_name_to_common_name(img.tags[0]),
-                     parse(img.attrs['Metadata']['LastTagTime']),
+                     datetime.strftime(parse(img.attrs['Metadata']['LastTagTime']),"%Y-%m-%d %H:%M:%S"),
                      img.labels['source_adapter'],
-                     img.labels['target_adapter'],)
-                     for img in shdocker.find_snowshu_images()]
+                     img.labels['target_adapter'],
+                     img.tags[0],)
+                     for img in images]
 
         return format_set_of_available_images(collection)      
                         
