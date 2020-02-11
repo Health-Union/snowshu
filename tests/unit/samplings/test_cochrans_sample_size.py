1f39f81df18640ba4ed0a65b1f69aa228be469ca
Ethan Knox <ethan.knox@health-union.com>
2020-02-11 09:26:55 -0500

    DATA-676 fixed /zero issue in sample sizes

--- tests/unit/samplings/test_cochrans_sample_size.py
+++ tests/unit/samplings/test_cochrans_sample_size.py
@@ -4,3 +4,8 @@ from snowshu.samplings.sample_sizes import CochransSampleSize
 def test_cochrans():
     sample_sizer=CochransSampleSize(0.04,0.95)
     assert sample_sizer.size(100000000) == 601
+
+def test_div_by_zero():
+    sample_sizer=CochransSampleSize(0.04,0.95)
+    assert sample_sizer.size(0) == 0
+
