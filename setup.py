4d41d46c226bf9c4ef81c4b1d9b90224460d0fb5
Chris Woodson <christopher.woodson@health-union.com>
2020-02-12 17:27:13 -0500

    publish alpha 0.0.1-a1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION='0.0.01'
+VERSION='0.0.1-a1'
 PYTHON_REQUIRES='3.7'
 
 packagedata=dict()
@@ -13,6 +13,7 @@ packagedata['name']="snowshu"
 packagedata['version']=VERSION
 packagedata['author']="Health Union Data Team"
 packagedata['author_email']='data@health-union.com'
+packagedata['url']='https://snowshu.readthedocs.io/en/master/index.html'
 packagedata['description']="Sample image management for data transform TDD."
 packagedata['classifiers']=['Development Status :: 3 - Alpha', "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"]
 packagedata['python_requires']=f'>={PYTHON_REQUIRES}'
@@ -22,6 +23,7 @@ packagedata['entry_points']=dict(console_scripts=['snowshu= snowshu.core.main:cl
 
 with open('./README.md','r') as readme:
     packagedata['long_description']=readme.read()
+    packagedata['long_description_content_type']='text/markdown'
 
 with open('./requirements/base.txt', 'r') as requirements:
     for line in requirements.readlines():
