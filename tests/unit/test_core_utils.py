047bd627bd779e7e2f1392eb15e43aa58d721305
Ethan Knox <ethan.knox@health-union.com>
2020-02-17 07:50:24 -0500

    test for base correct

--- tests/unit/test_core_utils.py
+++ tests/unit/test_core_utils.py
@@ -1,8 +1,30 @@
 import pytest
-from snowshu.core.utils import case_insensitive_dict_value
+from snowshu.core.utils import case_insensitive_dict_value,correct_case
 
 def test_case_insensitive_search():
 
     camelcased=dict(cOlUmn1=1, ColumN2=2)
 
     assert case_insensitive_dict_value(camelcased,'column1') == 1
+
+def test_correct_case():
+
+    correct=['upper','LOWER']
+    leave=['Upper','lOWER','Space Cased']
+
+    def correct_test_suite(under_test,upper):
+        for item in under_test:
+            expected = item.upper() if upper else item.lower()
+            assert correct_case(item,upper)== expected
+
+    def leave_test_suite(under_test,upper):
+        for item in under_test:
+            assert correct_case(item,upper)== item
+
+    for item in correct:
+        correct_test_suite(item,True)
+        correct_test_suite(item,False)
+
+    for item in leave:
+        leave_test_suite(item,True)
+        leave_test_suite(item,False)
