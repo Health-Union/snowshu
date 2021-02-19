1c1eba13934ac0bfe03d7b15825f1b262fcf3e2a
Chris Ulicny <chris.ulicny@health-union.com>
2021-02-19 16:40:50 -0500

    Cleaned up source adapter inheritance. Added overrides. Moved common functionality into base class

--- snowshu/adapters/source_adapters/snowflake_adapter.py
+++ snowshu/adapters/source_adapters/snowflake_adapter.py
@@ -1,12 +1,12 @@
-from concurrent.futures import ThreadPoolExecutor
 import time
 import pandas as pd
 import sqlalchemy
+from overrides import overrides
 from snowshu.exceptions import TooManyRecords
 from sqlalchemy.pool import NullPool
-from typing import List, Union, Any, Optional, Iterable
+from typing import List, Union, Any, Optional, Iterable, Tuple
 from snowshu.core.models.attribute import Attribute
-from snowshu.core.models.relation import Relation, at_least_one_full_pattern_match
+from snowshu.core.models.relation import Relation
 from snowshu.adapters.source_adapters import BaseSourceAdapter
 import snowshu.core.models.data_types as dtypes
 import snowshu.core.models.materializations as mz
@@ -72,7 +72,8 @@ class SnowflakeAdapter(BaseSourceAdapter):
     MATERIALIZATION_MAPPINGS = {"BASE TABLE": mz.TABLE,
                                 "VIEW": mz.VIEW}
 
-    def _get_all_databases(self) -> str:
+    @overrides
+    def _get_all_databases(self) -> List[str]:
         """ Use the SHOW api to get all the available db structures."""
         logger.debug('Collecting databases from snowflake...')
         show_result = tuple(self._safe_query("SHOW TERSE DATABASES")['name'].tolist())
@@ -80,73 +81,13 @@ class SnowflakeAdapter(BaseSourceAdapter):
         logger.debug(f'Done. Found {len(databases)} databases.')
         return databases
 
-    def _get_all_schemas(self, database: str) -> str:
+    @overrides
+    def _get_all_schemas(self, database: str) -> List[str]:
         logger.debug(f'Collecting schemas from {database} in snowflake...')
         show_result = self._safe_query(f'SHOW TERSE SCHEMAS IN DATABASE {database}')['name'].tolist()
         schemas = set(show_result)
         logger.debug(f'Done. Found {len(schemas)} schemas in {database} database.')
         return schemas
-
-    def _get_filtered_schemas(self, filters: Iterable[dict]):
-        """ Get all of the filtered schema structures based on the provided filters. """
-        db_filters = []
-        schema_filters = []
-        for f in filters:
-            new_filter = f.copy()
-            new_filter["name"] = ".*"
-            if schema_filters.count(new_filter) == 0:
-                schema_filters.append(new_filter)
-        for f in schema_filters:
-            new_filter = f.copy()
-            new_filter["schema"] = ".*"
-            if db_filters.count(new_filter) == 0:
-                db_filters.append(new_filter)
-
-        databases = self._get_all_databases()
-        database_relations = [Relation(self._correct_case(database), "", "", None, None) for database in databases]
-        filtered_databases = [rel for rel in database_relations if at_least_one_full_pattern_match(rel, db_filters)]
-
-        # get all schemas in all databases
-        filtered_schemas = []
-        for db_rel in filtered_databases:
-            schemas = self._get_all_schemas(database=db_rel.quoted(db_rel.database))
-            # TODO switch over to some internal class struct for this
-            schema_dicts = [
-                {
-                    "relation": Relation(db_rel.database, self._correct_case(schema), "", None, None),
-                    "raw_schema": schema
-                }
-                for schema in schemas
-            ]
-            filtered_schemas += [d for d in schema_dicts if at_least_one_full_pattern_match(d["relation"], schema_filters)]
-
-        return filtered_schemas
-
-    # TODO patterns is the same as _build_sum_patterns_from_config in graph.py
-    def build_catalog(self, patterns, thread_workers: int = 4):
-        filtered_schemas = self._get_filtered_schemas(patterns)
-
-        def accumulate_relations(schema_dict: dict, accumulator):
-            try:
-                rel = schema_dict["relation"]
-                quoted_db = rel.quoted(rel.database)
-                raw_schema = schema_dict["raw_schema"]
-                accumulator += self._get_relations_from_database(quoted_db, raw_schema)
-            except Exception as e:
-                logger.critical(e)
-                raise e
-
-        # get all columns for filtered db/schema
-        catalog = []
-        logger.info('Building filtered catalog...')
-        start_time = time.time()
-        with ThreadPoolExecutor(max_workers=thread_workers) as executor:
-            {executor.submit(accumulate_relations, d, catalog)
-            for d in filtered_schemas}
-
-        logger.info(
-            f'Done building catalog. Found a total of {len(catalog)} relations from the source in {duration(start_time)}.')
-        return tuple(catalog)
 
     def population_count_statement(self,relation:Relation)->str:
         """creates the count * statement for a relation
@@ -306,6 +247,7 @@ LIMIT {max_number_of_outliers})
             logger.error(message)
             raise NotImplementedError(message)
 
+    @overrides
     def _build_conn_string(self, overrides: Optional[dict] = {}) -> str:
         """overrides the base conn string."""
         conn_parts = [
@@ -320,7 +262,11 @@ LIMIT {max_number_of_outliers})
         get_string = "?" + "&".join([arg for arg in get_args])
         return (''.join(conn_parts)) + get_string
 
-    def _get_relations_from_database(self, database: str, schema: str) -> List[Relation]:
+    @overrides
+    def _get_relations_from_database(self, schema_obj: BaseSourceAdapter._DatabaseObject) -> List[Relation]:
+        quoted_database = schema_obj.full_relation.quoted(schema_obj.full_relation.database)  # quoted db name
+        relation_database = schema_obj.full_relation.database  # case corrected db name
+        case_sensitive_schema = schema_obj.case_sensitive_name  # case sensitive schame name
         relations_sql = f"""
                                  SELECT
                                     m.table_schema AS schema,
@@ -330,30 +276,30 @@ LIMIT {max_number_of_outliers})
                                     c.ordinal_position AS ordinal,
                                     c.data_type AS data_type
                                  FROM
-                                    {database}.INFORMATION_SCHEMA.TABLES m
+                                    {quoted_database}.INFORMATION_SCHEMA.TABLES m
                                  INNER JOIN
-                                    {database}.INFORMATION_SCHEMA.COLUMNS c
+                                    {quoted_database}.INFORMATION_SCHEMA.COLUMNS c
                                  ON
                                     c.table_schema = m.table_schema
                                  AND
                                     c.table_name = m.table_name
                                  WHERE
-                                    m.table_schema = '{schema}'
+                                    m.table_schema = '{case_sensitive_schema}'
                                     AND m.table_schema <> 'INFORMATION_SCHEMA'
                               """
 
         logger.debug(
-            f'Collecting detailed relations from database {database}...')
+            f'Collecting detailed relations from database {quoted_database}...')
         relations_frame = self._safe_query(relations_sql)
         unique_relations = (
             relations_frame['schema'] +
             '.' +
             relations_frame['relation']).unique().tolist()
         logger.debug(
-            f'Done collecting relations. Found a total of {len(unique_relations)} unique relations in database {database}')
+            f'Done collecting relations. Found a total of {len(unique_relations)} unique relations in database {quoted_database}')
         relations = list()
         for relation in unique_relations:
-            logger.debug(f'Building relation { database + "." + relation }...')
+            logger.debug(f'Building relation { quoted_database + "." + relation }...')
             attributes = list()
 
             for attribute in relations_frame.loc[(
@@ -366,7 +312,7 @@ LIMIT {max_number_of_outliers})
                         self._get_data_type(attribute.data_type)
                     ))
 
-            relation = Relation(self._correct_case(database),
+            relation = Relation(relation_database,
                                 self._correct_case(attribute.schema),
                                 self._correct_case(attribute.relation),
                                 self.MATERIALIZATION_MAPPINGS[attribute.materialization],
@@ -375,14 +321,16 @@ LIMIT {max_number_of_outliers})
             relations.append(relation)
 
         logger.debug(
-            f'Acquired {len(relations)} total relations from database {database}.')
+            f'Acquired {len(relations)} total relations from database {quoted_database}.')
         return relations
 
+    @overrides
     def _count_query(self, query: str) -> int:
         count_sql = f"WITH __SNOWSHU__COUNTABLE__QUERY as ({query}) SELECT COUNT(*) AS count FROM __SNOWSHU__COUNTABLE__QUERY"
         count = int(self._safe_query(count_sql).iloc[0]['count'])
         return count
 
+    @overrides
     def check_count_and_query(self, query: str,
                               max_count: int) -> pd.DataFrame:
         """checks the count, if count passes returns results as a dataframe."""
@@ -401,6 +349,7 @@ LIMIT {max_number_of_outliers})
         response = self._safe_query(query)
         return response
 
+    @overrides
     def get_connection(
             self,
             database_override: Optional[str] = None,
