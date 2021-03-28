c8734c1a56072473157122d4f3be7983ae8d52ef
Avinash Ankur <ankur112358@gmail.com>
2021-03-28 01:55:11 -0700

    setup tables for dev data set
    Sets up the db structure and loads tables from csv.
    Special tables (schema TESTS_DATA) are loded using sql commands from file, rest using dataframe.to_sql.
    
    Ref #30

--- tests/assets/integration_test_setup.py
+++ tests/assets/integration_test_setup.py
@@ -1,29 +1,35 @@
 #!/usr/local/python3
+import glob
 import logging
 import os
+import sys
+from pathlib import Path
+from sqlalchemy.exc import ProgrammingError
 
 import pandas as pd
 import sqlalchemy
 import yaml
-import logging
-import os
-import glob
-from pathlib import Path
+
 from snowshu.adapters.source_adapters.snowflake_adapter import SnowflakeAdapter
 from snowshu.core.models.credentials import Credentials
 
-# raise NotImplementedError('THIS IS A WORK IN PROGRESS, DO NOT USE!')
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler(sys.stdout))
+
 
 ASSETS_DIR = Path(os.path.dirname(__file__))
 DATA_DIR = ASSETS_DIR / 'data'
 CREDENTIALS = ASSETS_DIR / 'integration' / 'credentials.yml'
+SQL_CREATION_FILE = ASSETS_DIR / "data" / "data_types_snowflake_creation.sql"
+
 EXT = '*.csv'
 HARD_RESET = False
 TEST_DATABASE = "SNOWSHU_DEVELOPMENT"
 
 
 def get_connection_profile(credentials):
-    """ Used to get the source connection profile in dict form. 
+    """ Used to get the source connection profile in dict form.
 
         Raises:
             NotImplementedError: if source adapter is not implemented.
@@ -53,8 +59,8 @@ def get_all_csv_file():
     return all_csv_files
 
 
-def build_connectable() -> 'sqlalchemy.engine.base.Engine'::
-     """ Generates SQL alchemy engine from credentials. """
+def build_connectable() -> 'sqlalchemy.engine.base.Engine':
+    """ Generates SQL alchemy engine from credentials. """
     with open(CREDENTIALS) as cred_file:
         credentials = yaml.safe_load(cred_file)
 
@@ -64,22 +70,88 @@ def build_connectable() -> 'sqlalchemy.engine.base.Engine'::
     return adapter.get_connection()
 
 
+def setup_db_structure(all_data_files, engine):
+    """ Creates db objects like database and schema to be use later. """
+    LOGGER.info("Creating new DB objects if not present.")
+    dbs = set()
+    schemas = set()
+    tables = dict()
+
+    for data_file in all_data_files:
+        table_name = data_file.split('/')[-1].replace('.csv', '').upper()
+        schema_name = data_file.split('/')[-2].split('=')[-1]
+        database_name = data_file.split('/')[-3].split('=')[-1]
+        tables[".".join([database_name, schema_name, table_name])] = data_file
+        schemas.add(".".join([database_name, schema_name]))
+        dbs.add(database_name)
+
+    for database in dbs:
+        with engine.begin() as cursor:
+            try:
+                LOGGER.info(f"Creating DB - {database}")
+                cursor.execute(f"CREATE DATABASE {database};")
+            except ProgrammingError:
+                LOGGER.warning(
+                    f"{database} already present, Skipping creation.")
+
+    for schema in schemas:
+        with engine.begin() as cursor:
+            try:
+                LOGGER.info(f"Creating Schema - {schema}")
+                cursor.execute(f"CREATE SCHEMA {schema};")
+            except ProgrammingError:
+                LOGGER.warning(f"{schema} already present, Skipping creation.")
+
+    # remove special tables ISSUE #64
+    tables = dict(
+        filter(
+            lambda item: "TESTS_DATA" not in item[0],
+            tables.items()))
+    return tables
+
+
+def load_from_sql_file(engine):
+    """ Runs the SQL_CREATION_FILE. """
+    LOGGER.info("Using SQL file to load data to anti pattern tables.")
+    with open(SQL_CREATION_FILE) as sql_file:
+        sql_as_string = sql_file.read()
+        with engine.begin() as cursor:
+            # https://github.com/snowflakedb/snowflake-connector-net/issues/33
+            sql_strings = sql_as_string.split(';')
+            for sql_string in sql_strings:
+                cursor.execute(sql_string)
+
+
+def load_tables(tables, engine):
+    """ Iterates over the table names and paths to create and load data into tables. """
+    for full_table_name, csv_file in tables.items():
+        LOGGER.info("Processing %s", full_table_name)
+        with open(csv_file) as f:
+            frame = pd.read_csv(f)
+            table_name = csv_file.split('/')[-1].replace('.csv', '')
+            schema_name = csv_file.split('/')[-2].split('=')[-1]
+            use_schema_query = f"USE SCHEMA {TEST_DATABASE}.{schema_name};"
+            with engine.begin() as cursor:
+                cursor.execute(use_schema_query)
+                frame.to_sql(
+                    table_name,
+                    cursor,
+                    schema=schema_name,
+                    index=False,
+                    chunksize=16000,
+                    if_exists='fail')
+                LOGGER.debug("Data Loaded")
+
+
 if __name__ == "__main__":
     conn = build_connectable()
     if HARD_RESET:
-        with conn.begin() as connection:
-            LOGGER.warning(f"HARD_RESET set. Deleting {TEST_DATABASE} database.")
-            connection.execute(f"DROP DATABASE IF EXISTS {TEST_DATABASE};")
+        with conn.begin() as cursor:
+            LOGGER.warning(
+                f"HARD_RESET set. Deleting {TEST_DATABASE} database.")
+            cursor.execute(f"DROP DATABASE IF EXISTS {TEST_DATABASE};")
 
-    for csv_file in get_all_csv_file():
-        with open(csv_file) as f:
-            frame = pd.read_csv(f)
-            table_name = csv_file.split('/')[-1].replace('.csv', '').upper()
-            schema_name = csv_file.split('/')[-2].split('=')[-1]
-            database_name = csv_file.split('/')[-3].split('=')[-1]
-            frame.to_sql(
-                table_name,
-                conn,
-                schema=schema_name,
-                index=False,
-                chunksize=16000)
+    all_csv_files = get_all_csv_file()
+    tables = setup_db_structure(all_csv_files, conn)
+    load_from_sql_file(conn)
+    load_tables(tables, conn)
