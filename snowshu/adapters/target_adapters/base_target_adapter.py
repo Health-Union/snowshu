e42a1d137c136faa592b81bb738a2a9961c96d8e
Paul Fleischer <fleischer89@gmail.com>
2021-11-02 11:25:07 +0000

    Removing unused import

--- snowshu/adapters/target_adapters/base_target_adapter.py
+++ snowshu/adapters/target_adapters/base_target_adapter.py
@@ -8,7 +8,7 @@ import pandas as pd
 from snowshu.adapters import BaseSQLAdapter
 from snowshu.configs import (DEFAULT_INSERT_CHUNK_SIZE,
                              DOCKER_TARGET_CONTAINER, DOCKER_TARGET_PORT,
-                             IS_IN_DOCKER, DOCKER_REMOUNT_DIRECTORY)
+                             IS_IN_DOCKER)
 from snowshu.core.docker import SnowShuDocker
 from snowshu.core.models import Attribute, Credentials, Relation
 from snowshu.core.models import DataType, data_types as dt
