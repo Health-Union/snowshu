7b9996cc366955efd8902f9d934a5222074092ad
Paul Fleischer <fleischer89@gmail.com>
2021-12-09 12:41:11 +0000

    - Removing similar test

--- tests/integration/snowflake/test_end_to_end.py
+++ tests/integration/snowflake/test_end_to_end.py
@@ -404,26 +404,3 @@ def test_using_different_image(end_to_end):
     target_container.reload()
     assert target_container.status == 'running'
     target_container.remove(force=True)
-
-
-def test_incremental_build_with_override_image():
-    runner = CliRunner()
-    create_result = runner.invoke(cli, ('create', 
-                                        '--replica-file', 
-                                        INITIAL_INCREMENTAL_CONFIG_PATH))
-    if create_result.exit_code:
-        print(create_result.exc_info)
-        raise create_result.exception
-    create_output = create_result.output.split('\n')
-    assert any_appearance_of('Connected. Starting created container', create_output)
-
-    create_result = runner.invoke(cli, ('create', 
-                                        '--replica-file', 
-                                        CONFIGURATION_PATH, 
-                                        '--incremental', 
-                                        'snowshu_replica_integration-test'))
-    if create_result.exit_code:
-        print(create_result.exc_info)
-        raise create_result.exception
-    create_output = create_result.output.split('\n')
-    assert any_appearance_of('Connected. Starting created container', create_output)
