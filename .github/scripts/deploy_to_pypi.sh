2043722dfb8b5f9c00ebc259e71c92cd0ea104c6
Paul Fleischer <fleischer89@gmail.com>
2022-02-08 14:36:44 +0000

    DATA-2025: Configured test and live deployment to PyPI in separate workflows

--- .github/scripts/deploy_to_pypi.sh
+++ .github/scripts/deploy_to_pypi.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+action=$1
 # Upload package to PyPI using github secrets
-twine upload -u __token__ -p "$TWINE_PASSWORD" dist/*
+if [[ $action == "live_deploy" ]]; then
+    twine upload -u __token__ -p "$PYPI_KEY" dist/*
+else
+    twine upload -u __token__ -p "$TEST_PYPI_KEY" --repository testpypi dist/*
+fi
