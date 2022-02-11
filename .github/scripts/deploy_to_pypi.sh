e33864332e29b46b3c458ed4b260a8b541b548d9
Paul Fleischer <fleischer89@gmail.com>
2022-02-11 13:35:22 +0000

    DATA-2025: Refactoring workflows to use appropriate credentials per environment

--- .github/scripts/deploy_to_pypi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-action=$1
-# Upload package to PyPI using github secrets
-if [[ $action == "live_deploy" ]]; then
-    twine upload -u __token__ -p "$PYPI_KEY" dist/*
-else
-    twine upload -u __token__ -p "$TEST_PYPI_KEY" --repository testpypi dist/*
-fi
