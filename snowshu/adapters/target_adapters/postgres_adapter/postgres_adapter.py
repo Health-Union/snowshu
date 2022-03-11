8e3c74a0b7612e1092c633b42980ed03452b7def
Yaroslav Savonik <yaroslav.savonik@health-union.com>
2022-03-11 11:42:41 +0200

    DATA-2303 changed logic of creation schemas with FDWs

--- snowshu/adapters/target_adapters/postgres_adapter/postgres_adapter.py
+++ snowshu/adapters/target_adapters/postgres_adapter/postgres_adapter.py
@@ -275,20 +275,25 @@ class PostgresAdapter(BaseTargetAdapter):
             conn.execute(statement)
             logger.debug('Executed.')
 
-        snowshu_db = 'snowshu'
-        conn = self.get_connection(database_override=snowshu_db)
-        statement_runner('CREATE EXTENSION IF NOT EXISTS postgres_fdw')
-        for remote_database in filter((lambda x, current_db=snowshu_db: x != current_db), unique_databases):
-            statement_runner(f"CREATE SERVER IF NOT EXISTS {remote_database} FOREIGN DATA WRAPPER "
-                             f"postgres_fdw OPTIONS (dbname '{remote_database}',port '9999')")
+        for u_db in unique_databases:
+            conn = self.get_connection(database_override=u_db)
+            statement_runner('CREATE EXTENSION IF NOT EXISTS postgres_fdw')
+            for remote_database in filter((lambda x, current_db=u_db: x != current_db), unique_databases):
+                statement_runner(f"CREATE SERVER IF NOT EXISTS {remote_database} FOREIGN DATA WRAPPER "
+                                 f"postgres_fdw OPTIONS (dbname '{remote_database}',port '9999')")
 
-            statement_runner(f"CREATE USER MAPPING IF NOT EXISTS for snowshu SERVER {remote_database} "
-                             f"OPTIONS (user 'snowshu', password 'snowshu')")
+                statement_runner(f"CREATE USER MAPPING IF NOT EXISTS for snowshu SERVER {remote_database} "
+                                 f"OPTIONS (user 'snowshu', password 'snowshu')")
 
-        for schema_database, schema in unique_schemas:
-            if schema_database != snowshu_db:
-                statement_runner(f'DROP SCHEMA IF EXISTS {schema_database}__{schema} CASCADE')
-                statement_runner(f'CREATE SCHEMA {schema_database}__{schema}')
+            for schema_database, schema in unique_schemas:
+                if schema_database != u_db and not self.is_fdw_schema(schema, unique_databases):
+                    statement_runner(f'DROP SCHEMA IF EXISTS {schema_database}__{schema} CASCADE')
+                    statement_runner(f'CREATE SCHEMA {schema_database}__{schema}')
 
-                statement_runner(f'IMPORT FOREIGN SCHEMA {schema} FROM SERVER '
-                                 f'{schema_database} INTO {schema_database}__{schema}')
+                    statement_runner(f'IMPORT FOREIGN SCHEMA {schema} FROM SERVER '
+                                     f'{schema_database} INTO {schema_database}__{schema}')
+
+    @staticmethod
+    def is_fdw_schema(schema, unique_databases) -> bool:
+        splitted = schema.split('__')
+        return len(splitted) == 2 and splitted[0] in unique_databases
