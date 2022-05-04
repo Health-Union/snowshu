5898ff256395ca98e9429957b38988a8eefd7894
Halyna Tsekhmistro <halyna.tsekhmistro@health-union.com>
2022-05-04 11:12:45 +0300

    add integration and unit tests for postgres target adapter

--- tests/integration/snowflake/test_postgres_target_adapter.py
+++ tests/integration/snowflake/test_postgres_target_adapter.py
@@ -1,59 +1,127 @@
+import os
+
+import time
+from datetime import datetime
 from pandas.core.frame import DataFrame
-
+import docker
 import pytest
-import yaml
 
-import snowshu.core.models.materializations as mz
-from snowshu.core.models import data_types
+from click.testing import CliRunner
 from snowshu.adapters.target_adapters import BaseTargetAdapter
-from snowshu.core.models.attribute import Attribute
-from snowshu.core.models.materializations import TABLE
 from snowshu.adapters.target_adapters.postgres_adapter import PostgresAdapter
-from snowshu.configs import DOCKER_REMOUNT_DIRECTORY
-from snowshu.core.models.credentials import Credentials
-from snowshu.core.models.relation import Relation
-from tests.assets.integration_test_setup import CREDENTIALS, get_connection_profile
+from snowshu.configs import PACKAGE_ROOT
+from snowshu.core.main import cli
+from snowshu.core.models import Relation, Attribute, data_types
+from snowshu.core.models.materializations import TABLE
 from tests.common import rand_string
 
+CONFIGURATION_PATH = os.path.join(PACKAGE_ROOT, 'tests', 'assets', 'replica_test_config.yml')
+DOCKER_SPIN_UP_TIMEOUT = 15
 
-@pytest.fixture(scope='session')
-def pg_adapter():
-    adapter = PostgresAdapter(replica_metadata={})
-    return adapter
+@pytest.fixture(scope="session", autouse=True)
+def end_to_end(docker_flush_session):
+    runner = CliRunner()
 
+    create_result = runner.invoke(cli, ('create', '--replica-file', CONFIGURATION_PATH, '--barf'))
+    if create_result.exit_code:
+        print(create_result.exc_info)
+        raise create_result.exception
+    create_output = create_result.output.split('\n')
+    client = docker.from_env()
+    client.containers.run('snowshu_replica_integration-test',
+                          ports={'9999/tcp': 9999},
+                          name='integration-test',
+                          network='snowshu',
+                          detach=True)
+    time.sleep(DOCKER_SPIN_UP_TIMEOUT)  # the replica needs a second to initialize
+    return create_output
 
-def test_create_snowshu_schema_statement(pg_adapter):
-    assert pg_adapter._create_snowshu_schema_statement() == 'CREATE SCHEMA IF NOT EXISTS snowshu;'
+def test_create_database_if_not_exists(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    DATABASE = rand_string(10)
+    pg_adapter.create_database_if_not_exists(DATABASE)
+    database_list = pg_adapter._get_all_databases()
 
-
-def test_quoted(pg_adapter):
-    val = rand_string(10)
-    assert val == pg_adapter.quoted(val)
-
-
-def test_quoted_for_spaced_string(pg_adapter):
-    val = rand_string(5) + ' ' + rand_string(6)
-    assert f'"{val}"' == pg_adapter.quoted(val)
-
-
-def test_is_fdw_schema(pg_adapter):
-    schema = "DATASCIENCE_DEV"
-    unique_databases = ["HU_DATA", "SNOWFLAKE_SAMPLE_DATA", "DEMO_DB", "UTIL_DB"]
-    splitted = schema.split('__')
-    assert pg_adapter.is_fdw_schema(schema, unique_databases) == (len(splitted) == 2 and splitted[0] in unique_databases)
-
-
-def test_image_initialize_bash_commands(pg_adapter):
-    PRELOADED_PACKAGES = ['postgresql-plpython3-12']
-    commands = [f'apt-get update && apt-get install -y {" ".join(PRELOADED_PACKAGES)}']
-    assert pg_adapter.image_initialize_bash_commands().sort() == commands.sort()
+    assert DATABASE in database_list
 
 
-def test_build_snowshu_envars(pg_adapter):
-    snowshu_envars = ['POSTGRES_USER=snowshu',
-            'POSTGRES_PASSWORD=snowshu',
-            'POSTGRES_DB=snowshu',
-            f'PGDATA=/{DOCKER_REMOUNT_DIRECTORY}']
-    envars = [f"{envar}=snowshu" for envar in snowshu_envars]
-    envars.append(f"PGDATA=/{DOCKER_REMOUNT_DIRECTORY}")
-    assert envars == pg_adapter._build_snowshu_envars(snowshu_envars)
+def test_create_schema_if_not_exists(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    DATABASE, SCHEMA = [rand_string(10) for _ in range(2)]
+    pg_adapter.create_database_if_not_exists(DATABASE)
+    pg_adapter.create_schema_if_not_exists(DATABASE, SCHEMA)
+    list = pg_adapter._get_all_schemas(DATABASE)
+
+    assert SCHEMA in list
+
+
+def test_get_all_databases(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    db_list = pg_adapter._get_all_databases()
+    databases = ['postgres', 'snowshu', 'snowshu_development']
+
+    assert set(databases).issubset(set(db_list))
+
+
+def test_get_all_schemas(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    schemas_list = pg_adapter._get_all_schemas('snowshu_development')
+    schemas_set = {'polymorphic_data', 'external_data', 'tests_data', 'source_system'}
+
+    assert schemas_set.issubset(set(schemas_list))
+
+
+def test_get_relations_from_database(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    SCHEMA_OBJ = BaseTargetAdapter._DatabaseObject("snowshu",
+                                                   Relation("snowshu", "snowshu", "replica_meta", "", None))
+    relations_list = [Relation("snowshu", "snowshu", "replica_meta", TABLE, None)]
+    received_relations_list = pg_adapter._get_relations_from_database(schema_obj=SCHEMA_OBJ)
+
+    assert set(relations_list).issubset(set(received_relations_list))
+
+
+def test_create_all_database_extensions(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    extensions = 'citext'
+    pg_adapter.create_all_database_extensions()
+    statement = 'SELECT extname FROM pg_extension'
+    conn = pg_adapter.get_connection()
+    result = conn.execute(statement).fetchall()
+    extensions_list = [r[0] for r in result]
+
+    assert extensions in extensions_list
+
+
+def test_load_data_into_relation(end_to_end):
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    if pg_adapter.target != "localhost":
+        pg_adapter._credentials.host = 'integration-test'
+    
+    id_column = "id"
+    content_column = "content"
+    columns = [
+        Attribute(id_column, data_types.BIGINT),
+        Attribute(content_column, data_types.VARCHAR)
+    ]
+    relation = Relation("snowshu", "snowshu", "replica_meta", TABLE, columns)
+    relation.data = DataFrame({id_column:[1, 2, 3], content_column: [rand_string(5) for _ in range(3)]})
+    pg_adapter.load_data_into_relation(relation)
+
+    statement = f"SELECT * FROM snowshu.snowshu.replica_meta"
+    conn = pg_adapter.get_connection()
+    result = conn.execute(statement).fetchall()
+
+    assert len(result) == 3
--- tests/unit/test_postgres_target_adapter.py
+++ tests/unit/test_postgres_target_adapter.py
@@ -4,8 +4,9 @@ from snowshu.core.models import data_types
 from snowshu.core.models.attribute import Attribute
 from snowshu.core.models.materializations import TABLE
 from snowshu.adapters.target_adapters.postgres_adapter import PostgresAdapter
+from snowshu.configs import DOCKER_REMOUNT_DIRECTORY
 from snowshu.core.models.relation import Relation
-
+from tests.common import rand_string
 
 def test_x00_replacement():
     adapter = PostgresAdapter(replica_metadata={})
@@ -35,3 +36,46 @@ def test_x00_replacement():
     fixed_relation = adapter.replace_x00_values(relation)
     assert all(fixed_relation.data.loc[fixed_relation.data[id_col] == 1, [content_col]] == normal_val)
     assert all(fixed_relation.data.loc[fixed_relation.data[id_col] == 2, [content_col]] == f"weird{custom_replacement}value")
+
+def test_create_snowshu_schema_statement():
+    pg_adapter = PostgresAdapter(replica_metadata={})
+
+    assert pg_adapter._create_snowshu_schema_statement() == 'CREATE SCHEMA IF NOT EXISTS snowshu;'
+
+
+def test_quoted():
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    val = rand_string(10)
+    assert val == pg_adapter.quoted(val)
+
+    val = rand_string(5) + ' ' + rand_string(6)
+    assert f'"{val}"' == pg_adapter.quoted(val)
+
+
+def test_is_fdw_schema():
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    schema = "DATASCIENCE_DEV"
+    unique_databases = ["HU_DATA", "SNOWFLAKE_SAMPLE_DATA", "DEMO_DB", "UTIL_DB"]
+    splitted = schema.split('__')
+
+    assert pg_adapter.is_fdw_schema(schema, unique_databases) == (len(splitted) == 2 and splitted[0] in unique_databases)
+
+
+def test_image_initialize_bash_commands():
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    PRELOADED_PACKAGES = ['postgresql-plpython3-12']
+    commands = [f'apt-get update && apt-get install -y {" ".join(PRELOADED_PACKAGES)}']
+
+    assert pg_adapter.image_initialize_bash_commands().sort() == commands.sort()
+
+
+def test_build_snowshu_envars():
+    pg_adapter = PostgresAdapter(replica_metadata={})
+    snowshu_envars = ['POSTGRES_USER=snowshu',
+            'POSTGRES_PASSWORD=snowshu',
+            'POSTGRES_DB=snowshu',
+            f'PGDATA=/{DOCKER_REMOUNT_DIRECTORY}']
+    envars = [f"{envar}=snowshu" for envar in snowshu_envars]
+    envars.append(f"PGDATA=/{DOCKER_REMOUNT_DIRECTORY}")
+
+    assert set(envars) == set(pg_adapter._build_snowshu_envars(snowshu_envars))
