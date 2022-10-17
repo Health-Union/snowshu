7c360bf0ac22e7bb4c6f9474b49bb9fd77b73034
Serhii <serhii.kavierin@health-union.com>
2022-10-17 12:41:51 +0300

    DATA-3542: Refactored test

--- tests/integration/snowflake/test_incremental_build.py
+++ tests/integration/snowflake/test_incremental_build.py
@@ -19,6 +19,38 @@ INCREMENTAL_CONFIGURATION_PATH = os.path.join(
 CONFIGURATION_PATH = os.path.join(
     PACKAGE_ROOT, 'tests', 'assets', 'replica_test_config.yml')
 
+EXPECTED_BASE_TABLE_LIST = [
+    'address_region_attributes',
+    'case_testing',
+    'data_types',
+    'order_items',
+    'order_items_view',
+    'orders',
+    'products',
+    'social_users_import',
+    'user_addresses',
+    'user_cookies',
+    'users']
+
+EXPECTED_INREMENTED_TABLE_LIST = [
+    'address_region_attributes',
+    'case_testing',
+    'child_type_0_items',
+    'child_type_1_items',
+    'child_type_2_items',
+    'data_types',
+    'order_items',
+    'order_items_view',
+    'orders',
+    'parent_table',
+    'parent_table_2',
+    'products',
+    'social_users_import',
+    'user_addresses',
+    'user_cookies',
+    'users'
+]
+
 
 @pytest.fixture(scope="module")
 def base_for_incremental(docker_flush_module):
@@ -27,54 +59,28 @@ def base_for_incremental(docker_flush_module):
                   INCREMENTAL_CONFIGURATION_PATH, '--multiarch'))
 
 
-def test_incremental_build(base_for_incremental):
-    ''' Creates incremental builds of normal and multiarch variants, checks if they contain specified tables
-        WARNING: As of now, replica (config file, hostname, image and container name) for BASE is called INCREMENTAL, and vice versa.
-    '''
-    runner = CliRunner()
+def get_tables(hostname: str) -> list:
+    """Helper function that gets a list of tables from replica"""
+
+    conn_string = f'postgresql://snowshu:snowshu@{hostname}:9999/snowshu'
+    query_string = '''
+        select T.TABLE_NAME
+        from SNOWSHU.INFORMATION_SCHEMA.TABLES T
+        where T.TABLE_SCHEMA like 'snowshu_development%%'
+        order by 1
+        '''
+
+    conn = create_engine(conn_string)
+    query = conn.execute(query_string)
+    results = query.fetchall()
+    results = [row[0] for row in results]
+    return results
+
+
+def test_base_replica(base_for_incremental):
+    """Tests if base provided by 'base_for_inremental' is correct"""
+
     client = docker.from_env()
-
-    expected_base_table_list = [
-        'address_region_attributes',
-        'case_testing',
-        'data_types',
-        'order_items',
-        'order_items_view',
-        'orders',
-        'products',
-        'social_users_import',
-        'user_addresses',
-        'user_cookies',
-        'users']
-
-    expected_incremented_table_list = [
-        'address_region_attributes',
-        'case_testing',
-        'child_type_0_items',
-        'child_type_1_items',
-        'child_type_2_items',
-        'data_types',
-        'order_items',
-        'order_items_view',
-        'orders',
-        'parent_table',
-        'parent_table_2',
-        'products',
-        'social_users_import',
-        'user_addresses',
-        'user_cookies',
-        'users'
-    ]
-
-    def get_tables(hostname: str) -> list:
-        conn_string = f'postgresql://snowshu:snowshu@{hostname}:9999/snowshu'
-        query_string = '''select T.TABLE_NAME from SNOWSHU.INFORMATION_SCHEMA.TABLES T where T.TABLE_SCHEMA like 'snowshu_development%%' order by 1'''
-
-        conn = create_engine(conn_string)
-        query = conn.execute(query_string)
-        results = query.fetchall()
-        results = [row[0] for row in results]
-        return results
 
     for fake_local_arch in ['amd64', 'arm64']:
         with patch('snowshu.core.main.LOCAL_ARCHITECTURE', fake_local_arch) as _:
@@ -88,16 +94,27 @@ def test_incremental_build(base_for_incremental):
             time.sleep(DOCKER_SPIN_UP_TIMEOUT)
 
             assert get_tables(
-                'integration-test-incremental') == expected_base_table_list
+                'integration-test-incremental') == EXPECTED_BASE_TABLE_LIST
             container.remove(force=True)
 
-            # Do a native (of temporarily "native" arch) build
+
+def test_single_arch_incremental(base_for_incremental):
+    """Runs a 'create -i' command and tests the result
+       WARNING: replica tagged as snowshu_replica_integration-test-incremental is actually a base,
+                and replica tagged as snowshu_replica_integration-test is the result of incremental build
+    """
+
+    runner = CliRunner()
+    client = docker.from_env()
+
+    for fake_local_arch in ['amd64', 'arm64']:
+        with patch('snowshu.core.main.LOCAL_ARCHITECTURE', fake_local_arch) as _:
             runner.invoke(cli, ('create',
                                 '-i',
                                 f'snowshu_replica_integration-test-incremental:{fake_local_arch}',
                                 '--replica-file',
                                 CONFIGURATION_PATH),
-                                catch_exceptions=False)
+                          catch_exceptions=False)
 
             # Check if all images are present
             new_replicas_list = client.images.list(
@@ -113,22 +130,33 @@ def test_incremental_build(base_for_incremental):
             time.sleep(DOCKER_SPIN_UP_TIMEOUT)
 
             assert get_tables(
-                'integration-test') == expected_incremented_table_list
+                'integration-test') == EXPECTED_INREMENTED_TABLE_LIST
             container.remove(force=True)
-            
+
             # Remove new images before building another incremental
             for image in client.images.list('snowshu_replica_integration-test'):
                 for tag in image.tags:
                     client.images.remove(tag)
 
-            # Do a multiarch incremental build
+
+def test_multi_arch_incremental(base_for_incremental):
+    """Runs a 'create -i xxx -m' command and tests the result
+       WARNING: replica tagged as snowshu_replica_integration-test-incremental is actually a base,
+                and replica tagged as snowshu_replica_integration-test is the result of incremental build
+    """
+
+    runner = CliRunner()
+    client = docker.from_env()
+
+    for fake_local_arch in ['amd64', 'arm64']:
+        with patch('snowshu.core.main.LOCAL_ARCHITECTURE', fake_local_arch) as _:
             runner.invoke(cli, ('create',
                                 '-i',
                                 f'snowshu_replica_integration-test-incremental:{fake_local_arch}',
                                 '--replica-file',
                                 CONFIGURATION_PATH,
                                 '-m'),
-                                catch_exceptions=False)
+                          catch_exceptions=False)
 
             new_replicas_list = client.images.list(
                 'snowshu_replica_integration-test')
@@ -144,7 +172,7 @@ def test_incremental_build(base_for_incremental):
                 time.sleep(DOCKER_SPIN_UP_TIMEOUT)
 
                 assert get_tables(
-                    'integration-test') == expected_incremented_table_list
+                    'integration-test') == EXPECTED_INREMENTED_TABLE_LIST
                 container.remove(force=True)
 
             for image in client.images.list('snowshu_replica_integration-test'):
