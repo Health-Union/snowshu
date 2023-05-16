665dd0352b59863613965c69330832b81ad65134
Serhii <serhii.kavierin@health-union.com>
2023-05-16 16:56:39 +0300

    bugfix/DATA-4976: Fix and tests

--- tests/unit/test_postgres_target_adapter.py
+++ tests/unit/test_postgres_target_adapter.py
@@ -90,8 +90,16 @@ def test_build_snowshu_envars():
 def test_copy_replica_command():
     """Test whether copy replica command is correctly called"""
     pg_adapter = PostgresAdapter(replica_metadata={})
+    pg_adapter.stop_postgres = MagicMock()
+    # Skip copy if only one container
     pg_adapter.container = MagicMock()
-    pg_adapter.container.exec_run = MagicMock(return_value=(0, ANY))
+    pg_adapter.container.exec_run = MagicMock(return_value=MagicMock())
+    pg_adapter.copy_replica_data()
+    pg_adapter.container.exec_run.assert_not_called()
+
+    # Do copy if there are 2
+    pg_adapter.passive_container = MagicMock()
+    pg_adapter.passive_container.exec_run = MagicMock(return_value=MagicMock())
     pg_adapter.copy_replica_data()
     pg_adapter.container.exec_run.assert_called_with(
         f"/bin/bash -c 'cp -af $PGDATA/* {DOCKER_REPLICA_MOUNT_FOLDER}'",
