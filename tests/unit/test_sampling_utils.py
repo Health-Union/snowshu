a395a96137ab06e1a925ecd10504d9b8eb5eec62
Artem Karpenko <artem.karpenko@health-union.com>
2023-09-18 12:45:53 +0300

    DATA-5712: the brute_force_sampling was extended with max_allowed_rows option

--- tests/unit/test_sampling_utils.py
+++ tests/unit/test_sampling_utils.py
@@ -1,6 +1,17 @@
+import pytest
 from snowshu.core.samplings.utils import get_sampling_from_partial
-from snowshu.samplings.samplings import DefaultSampling
+from snowshu.samplings.samplings import DefaultSampling, BruteForceSampling
 
 
-def test_finds_default():
-    assert isinstance(get_sampling_from_partial('default'),DefaultSampling)
+@pytest.mark.parametrize('sample_method, expected_sampling', [
+    ('default', DefaultSampling),
+    ('brute_force', BruteForceSampling)
+])
+def test_finds_bruite_force(sample_method, expected_sampling):
+    """
+        GIVEN: sample_method - the sampling literal
+               expected_instance - the expected sampling class
+    """
+    assert isinstance(get_sampling_from_partial(sample_method), expected_sampling), f"The {expected_sampling.__name__} " \
+                                                                                  f"test case failed."
+
