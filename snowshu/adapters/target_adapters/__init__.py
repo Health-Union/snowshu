35dc929f81c413642bbc61a3fe2aecdc0a8e88bc
Kacper <kacper.wozniak@health-union.com>
2024-04-18 15:42:38 +0200

    feat/add-snowflake-target-adapter-class: add imports to __init__ files

--- snowshu/adapters/target_adapters/__init__.py
+++ snowshu/adapters/target_adapters/__init__.py
@@ -2,3 +2,4 @@ from .base_local_target_adapter import BaseLocalTargetAdapter
 from .base_remote_target_adapter import BaseRemoteTargetAdapter
 from .base_target_adapter import BaseTargetAdapter
 from .postgres_adapter import PostgresAdapter
+from .snowflake_adapter import SnowflakeAdapter
