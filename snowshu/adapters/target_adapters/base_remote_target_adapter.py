73da8b2cee5c0971182137180a5170f1d60922d1
Kacper <kacper.wozniak@health-union.com>
2024-04-22 11:43:32 +0200

    feat/add-snowflake-target-adapter-class: modify remote target adapter

--- snowshu/adapters/target_adapters/base_remote_target_adapter.py
+++ snowshu/adapters/target_adapters/base_remote_target_adapter.py
@@ -1,5 +1,24 @@
-from snowshu.adapters.target_adapters.base_target_adapter import BaseTargetAdapter
+import logging
+from io import StringIO
+from typing import Union
 
+from snowshu.core.configuration_parser import ConfigurationParser, TEMPLATES_PATH
+from snowshu.adapters.target_adapters.base_target_adapter import BaseTargetAdapter
+from snowshu.core.models import Credentials
+
+
+logger = logging.getLogger(__name__)
+
+CREDENTIALS_JSON_SCHEMA = TEMPLATES_PATH / 'credentials_schema_target.json'
 
 class BaseRemoteTargetAdapter(BaseTargetAdapter):
     """Base class for all remote target adapters eg. Snowflake, BigQuery etc."""
+
+    def _generate_credentials(self, creds: Union[str, 'StringIO', dict]) -> Credentials:
+        """ Check if credentials has been passed to credentials.yaml """
+        credentials = ConfigurationParser().get_dict_from_anything(creds, CREDENTIALS_JSON_SCHEMA)
+        credentials = credentials["targets"][0]
+        del credentials["name"]
+        del credentials["adapter"]
+        return Credentials(**credentials)
+
