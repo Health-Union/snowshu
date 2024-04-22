7f55e62c11cd6df6657f93075003552b18eecb3a
Kacper <kacper.wozniak@health-union.com>
2024-04-22 11:41:40 +0200

    feat/add-snowflake-target-adapter-class: add snowflake target adapter

---
+++ snowshu/adapters/target_adapters/snowflake_adapter/snowflake_adapter.py
@@ -0,0 +1,50 @@
+import logging
+
+from snowshu.adapters.snowflake_common import SnowflakeCommon
+from snowshu.core.configuration_parser import Configuration
+from snowshu.core.models.credentials import (
+    USER,
+    PASSWORD,
+    ACCOUNT,
+    DATABASE,
+    SCHEMA,
+    WAREHOUSE,
+    ROLE,
+)
+from snowshu.adapters.target_adapters.base_remote_target_adapter import (
+    BaseRemoteTargetAdapter,
+)
+
+logger = logging.getLogger(__name__)
+
+class SnowflakeAdapter(SnowflakeCommon, BaseRemoteTargetAdapter):
+    REQUIRED_CREDENTIALS = (
+        USER,
+        PASSWORD,
+        ACCOUNT,
+        DATABASE,
+    )
+    ALLOWED_CREDENTIALS = (
+        SCHEMA,
+        WAREHOUSE,
+        ROLE,
+    )
+    MATERIALIZATION_MAPPINGS = {}
+
+    def __init__(self, replica_metadata: dict):
+        super().__init__(replica_metadata)
+
+    def initialize_replica(self, config: Configuration, **kwargs):
+        pass
+
+    def _initialize_snowshu_meta_database(self):
+        pass
+
+    def create_database_if_not_exists(self):
+        pass
+
+    def create_or_replace_view(self):
+        pass
+
+    def create_schema_if_not_exists(self):
+        pass
