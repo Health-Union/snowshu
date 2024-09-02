609b9c11298129974087ca31e7ce4dc5b96e6486
Kacper <kacper.wozniak@health-union.com>
2024-09-02 15:46:29 +0200

    DATA-7099: fix linter

--- snowshu/adapters/target_adapters/base_target_adapter.py
+++ snowshu/adapters/target_adapters/base_target_adapter.py
@@ -132,7 +132,7 @@ class BaseTargetAdapter(BaseSQLAdapter):
             raise
 
         logger.info(final_message)
- 
+
     def clone_relation(self, relation: Relation) -> None:
         """Clones a relation in the target.
         Args:
