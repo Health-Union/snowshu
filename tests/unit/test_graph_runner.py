484adf4a54988565a0ce3a9b193a1d1ce7f899d7
Kacper <kacper.wozniak@health-union.com>
2024-09-02 14:15:22 +0200

    DATA-7099: fix test

--- tests/unit/test_graph_runner.py
+++ tests/unit/test_graph_runner.py
@@ -73,7 +73,7 @@ def test_traverse_and_execute_custom_max_rows_pass(stub_graph_set):
     runner.barf=False
     graph_set,vals=stub_graph_set
     source_adapter.scalar_query.return_value=1000
-    source_adapter.check_count_and_query.return_value=pd.DataFrame([dict(population_size=1000,sample_size=100)])
+    source_adapter.check_count_and_query.return_value = (pd.DataFrame([dict(population_size=1000, sample_size=100)]), 100)
     dag=copy.deepcopy(graph_set[-1])  # last graph in the set is the dag
 
     def fake_data(self, val: pd.DataFrame):
