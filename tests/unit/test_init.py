4084f0740287d74fe1d1635470965d31153b39b5
Kacper <kacper.wozniak@health-union.com>
2024-09-11 15:08:14 +0200

    fix test

--- tests/unit/test_init.py
+++ tests/unit/test_init.py
@@ -7,6 +7,7 @@ import pytest
 from click.testing import CliRunner
 
 from snowshu.core import main
+from snowshu.configs import ARCH_MAP
 from snowshu.core.graph import SnowShuGraph
 from snowshu.core.graph_set_runner import GraphSetRunner
 from snowshu.core.replica.replica_factory import ReplicaFactory
