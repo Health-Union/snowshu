df0f2576a6d0e0a7ea1dd98f77f7b65562df01a7
Kacper <kacper.wozniak@health-union.com>
2024-09-27 14:57:45 +0200

    fix linter issues

--- snowshu/adapters/target_adapters/snowflake_adapter/snowflake_adapter.py
+++ snowshu/adapters/target_adapters/snowflake_adapter/snowflake_adapter.py
@@ -153,8 +153,8 @@ class SnowflakeAdapter(SnowflakeCommon, BaseRemoteTargetAdapter):
             )
             return []
 
-    def _get_relations_from_database(
-        self, database: str, schema: str  # pylint: disable=arguments-differ
+    def _get_relations_from_database( # pylint: disable=arguments-differ
+        self, database: str, schema: str  
     ) -> List[Relation]:
         """Retrieve all relations from a given database and schema.
 
