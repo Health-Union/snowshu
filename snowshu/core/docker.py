fce47d1322a73947930ec822e9da2dc412106b0e
glysav <yaroslav.savonik@globallogic.com>
2024-12-16 15:00:38 +0200

    hotfix/check-container-logs: check container logs adn status

--- snowshu/core/docker.py
+++ snowshu/core/docker.py
@@ -274,8 +274,16 @@ class SnowShuDocker:
             raise
         logger.info(
             f'Container {container.name} started, running initial setup...')
-        logger.info(f'{container}\nContainer name {container.name}\nContainer status {container.status}\nContainer logs {list(container.logs)}')
+        logger.info(f'{container}\nContainer name {container.name}\nContainer status {container.status}')
+        logger.info('Container logs:')
+        for log in self.client.logs(container.id):
+            logger.info(f'log: {log}')
+        logger.info('Running _run_container_setup')
         self._run_container_setup(container, target_adapter)
+        logger.info('Completed _run_container_setup')
+        logger.info('Container logs:')
+        for log in self.client.logs(container.id):
+            logger.info(f'log: {log}')
         logger.info(f'Container {container.name} fully initialized.')
 
         return container
